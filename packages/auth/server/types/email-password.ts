a6ce25947f76a7a7c15ef9c89d5d61cb89f09649
Mohammad Abuzar <abuzarmohammad945@gmail.com>
2025-04-23 16:14:33 +0500

    update backend to link with nomia , Chnaged Signup flow in Documenso

--- packages/auth/server/types/email-password.ts
+++ packages/auth/server/types/email-password.ts
@@ -19,19 +19,7 @@ export const ZPasswordSchema = z
   .string()
   .min(8, { message: 'Must be at least 8 characters in length' })
   .max(72, { message: 'Cannot be more than 72 characters in length' })
-  .refine((value) => value.length > 25 || /[A-Z]/.test(value), {
-    message: 'One uppercase character',
-  })
-  .refine((value) => value.length > 25 || /[a-z]/.test(value), {
-    message: 'One lowercase character',
-  })
-  .refine((value) => value.length > 25 || /\d/.test(value), {
-    message: 'One number',
-  })
-  .refine((value) => value.length > 25 || /[`~<>?,./!@#$%^&*()\-_"'+=|{}[\];:\\]/.test(value), {
-    message: 'One special character is required',
-  });
-
+  .regex(/[a-zA-Z]/, { message: 'Must contain at least one letter' })
 export const ZSignUpSchema = z.object({
   name: z.string().min(1),
   email: z.string().email(),
