b61dc8e385e69ce164aad774008a2705f70d1300
Mohammad Abuzar <abuzarmohammad945@gmail.com>
2025-04-25 10:15:44 +0500

    typescript issues resolved

--- packages/email/template-components/template-footer.tsx
+++ packages/email/template-components/template-footer.tsx
@@ -25,7 +25,7 @@ export const TemplateFooter = ({ isDocument = true }: TemplateFooterProps) => {
 
       {branding.brandingCompanyDetails ? (
         <Text className="my-8 text-sm text-slate-400">
-          {branding.brandingCompanyDetails.split('\n').map((line, idx) => {
+          {branding.brandingCompanyDetails.split('\n').map((line :any, idx:any ) => {
             return (
               <>
                 {idx > 0 && <br />}
