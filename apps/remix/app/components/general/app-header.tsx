1043b293d9328c63d840a50cdcbbb0d3e129625b
haseebmanzoorcbt <haseeb.manzoor@thecherrybyte.com>
2025-05-13 11:55:21 +0500

    --STANDALONE ESIGN push 8

--- apps/remix/app/components/general/app-header.tsx
+++ apps/remix/app/components/general/app-header.tsx
@@ -71,7 +71,7 @@ export const Header = ({ className, user, teams, ...props }: HeaderProps) => {
       {...props}
     >
       <div className="mx-auto flex w-full max-w-screen-xl items-center justify-between gap-x-4 px-4 md:justify-normal md:px-8">
-        {!internal || isInternalLS ? (
+        {!internal || !isInternalLS ? (
           <Link
             to={`${getRootHref(params, { returnEmptyRootString: true })}/documents`}
             className="focus-visible:ring-ring ring-offset-background hidden rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 md:inline"
@@ -84,38 +84,37 @@ export const Header = ({ className, user, teams, ...props }: HeaderProps) => {
           </div>
         )}
 
-        {!internal ||
-          (!isInternalLS && <AppNavDesktop setIsCommandMenuOpen={setIsCommandMenuOpen} />)}
+        {!internal || !isInternalLS ? (
+          <AppNavDesktop setIsCommandMenuOpen={setIsCommandMenuOpen} />
+        ) : null}
 
-        {!internal ||
-          (!isInternalLS && (
-            <div
-              className="flex gap-x-4 md:ml-8"
-              title={selectedTeam ? selectedTeam.name : (user.name ?? '')}
-            >
-              <MenuSwitcher user={user} teams={teams} />
-            </div>
-          ))}
+        {!internal || !isInternalLS ? (
+          <div
+            className="flex gap-x-4 md:ml-8"
+            title={selectedTeam ? selectedTeam.name : (user.name ?? '')}
+          >
+            <MenuSwitcher user={user} teams={teams} />
+          </div>
+        ) : null}
 
-        {!internal ||
-          (!isInternalLS && (
-            <div className="flex flex-row items-center space-x-4 md:hidden">
-              <button onClick={() => setIsCommandMenuOpen(true)}>
-                <SearchIcon className="text-muted-foreground h-6 w-6" />
-              </button>
+        {!internal || !isInternalLS ? (
+          <div className="flex flex-row items-center space-x-4 md:hidden">
+            <button onClick={() => setIsCommandMenuOpen(true)}>
+              <SearchIcon className="text-muted-foreground h-6 w-6" />
+            </button>
 
-              <button onClick={() => setIsHamburgerMenuOpen(true)}>
-                <MenuIcon className="text-muted-foreground h-6 w-6" />
-              </button>
+            <button onClick={() => setIsHamburgerMenuOpen(true)}>
+              <MenuIcon className="text-muted-foreground h-6 w-6" />
+            </button>
 
-              <AppCommandMenu open={isCommandMenuOpen} onOpenChange={setIsCommandMenuOpen} />
+            <AppCommandMenu open={isCommandMenuOpen} onOpenChange={setIsCommandMenuOpen} />
 
-              <AppNavMobile
-                isMenuOpen={isHamburgerMenuOpen}
-                onMenuOpenChange={setIsHamburgerMenuOpen}
-              />
-            </div>
-          ))}
+            <AppNavMobile
+              isMenuOpen={isHamburgerMenuOpen}
+              onMenuOpenChange={setIsHamburgerMenuOpen}
+            />
+          </div>
+        ) : null}
       </div>
     </header>
   );
