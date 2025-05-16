68c9f05be7653144a208c2d75f43fc4d9f30c389
Mohammad Abuzar <abuzarmohammad945@gmail.com>
2025-05-16 17:48:38 +0500

    standalone esign app types error

--- packages/lib/server-only/paystack/paystack-api.d.ts
+++ packages/lib/server-only/paystack/paystack-api.d.ts
@@ -1,1 +1,7 @@
-declare module 'paystack-api'; 
+declare module 'paystack-api' {
+  // You can provide a basic or custom type declaration for Paystack here.
+  // For example, if you're using it as a default export, you can declare it as:
+  
+  const Paystack: any;
+  export default Paystack;
+}
