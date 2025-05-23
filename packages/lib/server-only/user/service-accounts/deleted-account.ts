cffad9d77cc294214cac753744873be6a3071518
Mohammad Abuzar <abuzarmohammad945@gmail.com>
2025-05-23 14:18:37 +0500

    Merge #18 : Subscriptions Reload + Service Account+1

--- packages/lib/server-only/user/service-accounts/deleted-account.ts
+++ packages/lib/server-only/user/service-accounts/deleted-account.ts
@@ -1,9 +1,9 @@
 import { prisma } from '@documenso/prisma';
 
-export const deletedAccountServiceAccount = async (email: string) => {
+export const deletedAccountServiceAccount = async (email_to_use: string) => {
   const serviceAccount = await prisma.user.findFirst({
     where: {
-      email,
+      email: email_to_use,
     },
   });
 
