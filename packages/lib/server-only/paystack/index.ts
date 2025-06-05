192df1e79a0b6e8c099c12ff1037737bd35009a7
Mohammad Abuzar <abuzarmohammad945@gmail.com>
2025-06-05 11:22:13 +0500

    Live plans and key updated

--- packages/lib/server-only/paystack/index.ts
+++ packages/lib/server-only/paystack/index.ts
@@ -3,9 +3,7 @@ import { Paystack } from 'paystack-sdk';
 import { env } from '../../utils/env';
 import { any } from 'zod';
 
-
-
-const paystack = new Paystack("sk_test_be0b3cb028d5ea5cdf6aa15c2a60a9c9b453dba0");
+const paystack = new Paystack(env('NEXT_PAYSTACK_LIVE_KEY') ?? env('NEXT_PAYSTACK_TEST_KEY') ?? '');
 
 export { paystack };
 
