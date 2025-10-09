75ae6450cc37c4c9e51f9d88956baf1ccc4f2f49
Mohammad Abuzar <abuzarmohammad945@gmail.com>
2025-10-09 17:07:54 +0500

    api tokens

--- apps/remix/app/routes/_authenticated+/settings+/tokens.tsx
+++ apps/remix/app/routes/_authenticated+/settings+/tokens.tsx
@@ -24,19 +24,7 @@ export default function ApiTokensPage() {
     <div>
       <SettingsHeader
         title={<Trans>API Tokens</Trans>}
-        subtitle={
-          <Trans>
-            On this page, you can create and manage API tokens. See our{' '}
-            <a
-              className="text-primary underline"
-              href={'https://docs.documenso.com/developers/public-api'}
-              target="_blank"
-            >
-              Documentation
-            </a>{' '}
-            for more information.
-          </Trans>
-        }
+        subtitle={<Trans>On this page, you can create and manage API tokens.</Trans>}
       />
 
       {team && team?.currentTeamMember.role !== TeamMemberRole.ADMIN ? (
