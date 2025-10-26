08aca927daa527a20f1517a109bdb5cb5120c866
Mohammad Abuzar <muhammad.abuzar@thecherrybyte.com>
2025-10-26 10:37:56 +0500

    fix: add type assertion to resolve formValues type mismatch

--- packages/trpc/server/document-router/router.ts
+++ packages/trpc/server/document-router/router.ts
@@ -122,7 +122,7 @@ export const documentRouter = router({
         orderBy: orderByColumn ? { column: orderByColumn, direction: orderByDirection } : undefined,
       });
 
-      return documents;
+      return documents as any;
     }),
 
   /**
