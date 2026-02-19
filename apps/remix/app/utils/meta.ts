827109c1507a3251c1939178e3fc3e907961efb7
Mohammad Abuzar <135220443+abuzar-dev@users.noreply.github.com>
2026-02-19 12:43:01 +0500

    certificate changes

--- apps/remix/app/utils/meta.ts
+++ apps/remix/app/utils/meta.ts
@@ -1,12 +1,13 @@
 import { NEXT_PUBLIC_WEBAPP_URL } from '@documenso/lib/constants/app';
+import { env } from '@documenso/lib/utils/env';
 
 export const appMetaTags = (title?: string) => {
   const description =
-    'Streamline your document signing with Nomia’s intuitive e-signature platform. Quick, easy, and secure, our solution makes e-signing effortless and affordable. Create reusable templates, manage individual or bulk signings, track document status, and send reminders—all within a seamless, integrated workflow.';
+    'An intuitive, affordable app with pay-as-you-go pricing bundles and unlimited seats. Premium e-sign without the premium price tag. Fully AATL compliant.';
 
   return [
     {
-      title: "Nomia Signatures",
+      title: title ? `${title} - Nomia Signatures` : 'Nomia Signatures',
     },
     {
       name: 'description',
@@ -15,11 +16,11 @@ export const appMetaTags = (title?: string) => {
     {
       name: 'keywords',
       content:
-        'Nomia Signatures, DocuSign alternative, document signing, open signing infrastructure, open-source community, fast signing, beautiful signing, smart templates  ,fast and secure signing platform , nomiadocs',
+        'Nomia Signatures, e-signature platform, nomiadocs, nomia, DocuSign alternative, document signing, open signing infrastructure, open-source community, fast signing, beautiful signing, smart templates',
     },
     {
       name: 'author',
-      content: 'Nomia Signatures, Inc.',
+      content: 'Nomia Africa (Pty) Limited',
     },
     {
       name: 'robots',
@@ -27,35 +28,19 @@ export const appMetaTags = (title?: string) => {
     },
     {
       property: 'og:title',
-      content: 'Nomia E-Signatures – Fast, Smart, and Secure',
+      content: 'Nomia | E-Sign App with Unlimited Seats',
     },
     {
       property: 'og:description',
       content: description,
     },
-    // {
-    //   property: 'og:image',
-    //   content: `${NEXT_PUBLIC_WEBAPP_URL()}/opengraph-image.jpg`,
-    // },
     {
       property: 'og:type',
       content: 'website',
     },
-    // {
-    //   name: 'twitter:card',
-    //   content: 'summary_large_image',
-    // },
-    // {
-    //   name: 'twitter:site',
-    //   content: '@documenso',
-    // },
-    // {
-    //   name: 'twitter:description',
-    //   content: description,
-    // },
-    // {
-    //   name: 'twitter:image',
-    //   content: `${NEXT_PUBLIC_WEBAPP_URL()}/opengraph-image.jpg`,
-    // },
+    {
+      name: 'twitter:description',
+      content: description,
+    }
   ];
 };
